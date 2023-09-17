1ad97f32d3a5fc00b159a7182ab3376c9311f839
Zelimkhan-P <chuvokhi@gmail.com>
2023-09-17 19:50:00 +0300

    start_ad.sh

---
+++ scripts/start_ad.sh
@@ -0,0 +1,7 @@
+
+route add -net 10.0.1.0 netmask 255.255.255.0 gw wg
+cat /ssh/key.pub > /root/.ssh/authorized_keys
+ssh-keygen -A
+exec /usr/sbin/sshd -D -e "$@" &
+/AdGuardHome/AdGuardHome --pidfile /AdGuardHome/pid -c /opt/adguardhome/AdGuardHome.yaml -h 0.0.0.0 -w /opt/adguardhome/
+tail -f /dev/null
